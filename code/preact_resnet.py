26e158cd3c6b3c5ed9b87bb7bf573113af2d6747
Sahar Abdelnabi <59022078+S-Abdelnabi@users.noreply.github.com>
2022-01-30 18:59:39 +0100

    Add files via upload

--- code/preact_resnet.py
+++ code/preact_resnet.py
@@ -13,8 +13,6 @@ class PreActBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.relu =  nn.ReLU(inplace=True)
-        #self.identity = nn.Identity()
 
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -22,14 +20,11 @@ class PreActBlock(nn.Module):
             )
 
     def forward(self, x):
+        out = F.relu(self.bn1(x))
         shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
-        out = self.relu(self.bn1(x))
         out = self.conv1(out)
-        out = self.relu(self.bn2(out))
-        out = self.conv2(out)
-        out = out + shortcut
-        #out = self.identity(out)
-        #out += shortcut
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += shortcut
         return out
 
 
@@ -71,7 +66,6 @@ class PreActResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.bn = nn.BatchNorm2d(512 * block.expansion)
-        self.relu =  nn.ReLU(inplace=True)
         self.linear = nn.Linear(512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -88,7 +82,7 @@ class PreActResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = self.relu(self.bn(out))
+        out = F.relu(self.bn(out))
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
